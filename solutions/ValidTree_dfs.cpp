f7ff7dfbfa42e240c217e2b073c3b20bfaf41fde
zosia <rose.lindgom@gmail.com>
2024-11-17 13:55:41 +0900

    Valid Tree with dfs

---
+++ solutions/ValidTree_dfs.cpp
@@ -0,0 +1,57 @@
+/*
+    Check if given graph is a valid tree (Find and Union)
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+const int N = 100000+21;
+vector<int> graph[N];
+int vis[N];
+int n, m, a, b;
+
+bool dfs(int v, int prev)
+{
+    if (vis[v])
+        return false;
+    bool ans = true;
+    vis[v] = 1;
+    for (int i=0; i<graph[v].size(); i++)
+    {
+        int curr = graph[v][i];
+        if (curr != prev && dfs(curr, v)==false)
+            ans = false;
+    }
+    return ans;
+}
+
+int main()
+{
+	cin>>n>>m;
+    if (m == n-1)
+    {
+    	for (int i=0; i<m; i++)
+        {
+            cin>>a>>b;
+            graph[a].push_back(b);
+            graph[b].push_back(a);
+        }
+        bool odp = dfs(0, -1);
+        if (odp)
+        {
+            cout<<"PRAWDA\n";
+        }
+        else
+        {
+            cout<<"FAŁSZ\n";
+        }
+        }
+    else
+    {
+        for (int i=0; i<m; i++)
+        {
+            cin>>a>>b;
+        }
+        cout<<"FALSZ\n";
+    }
+}
