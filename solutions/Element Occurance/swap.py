5e9b1ccd45173c88998b98c5e2463badde38535c
zosia <rose.lindgom@gmail.com>
2025-01-11 18:51:13 +0900

    Element Occurance

---
+++ solutions/Element Occurance/swap.py
@@ -0,0 +1,35 @@
+"""
+    We are given an integer m (1 < m ≤ 1,000,000) and two arrays, A and B, 
+    each containing n integers: 
+    A = [a0, a1, ..., an-1] and B = [b0, b1, ..., bn-1], 
+    where 0 < ai, bi < m.
+
+    We need to determine whether there exists a swap operation that can be 
+    performed on these arrays such that, after the swap, the sum of elements 
+    in array A is equal to the sum of elements in array B.
+
+    A "swap operation" means selecting one element from array A and one element 
+    from array B and exchanging their values.
+"""
+
+def find_swap(A, B):
+    sumA = sum(A)
+    sumB = sum(B)
+
+    diff = sumA - sumB
+    setB = set(B)
+
+    if diff % 2 != 0:
+        return False
+    
+    # for each element a in A, we are looking if there exists a b in B such that
+    # a = b + diff / 2
+    for a in A:
+        if (a - diff // 2) in setB:
+            return True
+    return False
+
+A = list(map(int, input().split(' ')))
+B = list(map(int, input().split(' ')))
+
+print(find_swap(A, B))
