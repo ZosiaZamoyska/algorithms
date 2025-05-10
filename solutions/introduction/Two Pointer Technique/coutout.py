97776701517b7014fbcc91c9fb2f6c679d3d5d52
zosia <rose.lindgom@gmail.com>
2025-05-10 18:46:25 +0900

    coutout

---
+++ solutions/introduction/Two Pointer Technique/coutout.py
@@ -0,0 +1,30 @@
+def longest_subarray_with_sum(n, s, nums):
+    prefix = [0] * (n + 1)
+    for i in range(n):
+        prefix[i + 1] = prefix[i] + nums[i]
+
+    prefix_with_index = [(prefix[i], i) for i in range(n + 1)]
+    prefix_with_index.sort()
+
+    max_len = 0
+    i = 0
+    j = 0
+    while j < len(prefix_with_index):
+        current_diff = prefix_with_index[j][0] - prefix_with_index[i][0]
+        if current_diff < s:
+            j += 1
+        elif current_diff > s:
+            i += 1
+        else:
+            idx_i = prefix_with_index[i][1]
+            idx_j = prefix_with_index[j][1]
+            if idx_j > idx_i:
+                max_len = max(max_len, idx_j - idx_i)
+            j += 1
+
+    return max_len
+
+n, s = map(int, input().split())
+nums = list(map(int, input().split()))
+
+print(longest_subarray_with_sum(n, s, nums))
