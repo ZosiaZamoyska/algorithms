c69067620bc28ff09b05fb68ba88fc6e5310af90
zosia <rose.lindgom@gmail.com>
2025-06-14 21:49:46 +0900

    dfs py

---
+++ implementation/dfs.py
@@ -0,0 +1,42 @@
+'''
+    8 8
+    1 2
+    2 3
+    3 4
+    1 6
+    6 3
+    3 7
+    7 8
+    8 4
+'''
+N = 1000 * 1000 + 6
+as_ = [[] for _ in range(N)]
+vis = [False] * N
+pre = [0] * N
+post = [0] * N
+counter_pre = 1
+counter_post = 1
+
+def dfs(curr):
+    global counter_pre, counter_post
+    vis[curr] = True
+    pre[counter_pre] = curr
+    counter_pre += 1
+
+    for i in as_[curr]:
+        if not vis[i]:
+            dfs(i)
+
+    post[counter_post] = curr
+    counter_post += 1
+
+x, y = map(int, input().split())
+for _ in range(y):
+    from_, to = map(int, input().split())
+    as_[from_].append(to)
+    as_[to].append(from_)
+
+dfs(1)
+
+print(' '.join(str(pre[i]) for i in range(1, x + 1)))
+print(' '.join(str(post[i]) for i in range(1, x + 1)))
